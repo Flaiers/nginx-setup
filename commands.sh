26cb499320240590c359209ab3372850277c2d06
Max <bigin.maks@gmail.com>
2022-04-09 22:19:32 +0300

    certbot

--- commands.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt install snapd
-sudo snap install core; sudo snap refresh core
-sudo apt-get remove certbot
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot certonly --nginx --config-dir $HOME/letsencrypt
-sudo chown -R $USER:$USER $HOME/letsencrypt
